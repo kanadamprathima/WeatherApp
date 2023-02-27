e83e8c4131ba696e84616664feb45fe14025f030
prathima <kanadamprathima@gmail.com>
2023-02-27 16:52:15 +0100

    fetch data from openweatherapi

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,22 @@
 <template>
   <div class="container my-5">
     <h1 class="title text-center">Weather Forecast</h1>
-    <form>
+    <form class="search-location" v-on:submit.prevent="getWeather">
       <input
         type="text"
         placeholder="Enter location"
-        auto-complete="on"
-        class="form-control text-muted form-rounded p-3 shadow"
+        class="form-control text-muted form-rounded p-4 shadow-sm"
+        v-model="citySearch"
+        auto-complete="off"
       />
     </form>
     <div class="card rounded my-4 shadow-lg back-card overflow:hidden">
       <!-- top of card starts here -->
       <div class="card-top text-center" style="margin-bottom: 15rem">
         <div class="city-name my-3">
-          <p>INDIA</p>
+          <p>{{ Weather.cityName }}</p>
           <span>....</span>
-          <p class="">IND</p>
+          <p class="">{{ Weather.country }}</p>
         </div>
       </div>
       <!-- card middle body -->
@@ -23,15 +24,15 @@
         <div class="card-mid">
           <div class="row">
             <div class="col-12 text-center temp">
-              <span>20&deg;C</span>
-              <p class="my-4">clouds everywhere</p>
+              <span>{{ Weather.temperature }}&deg;C</span>
+              <p class="my-4">{{ Weather.description }}</p>
             </div>
           </div>
           <div class="row">
             <div class="col d-flex justify-content-between px-5 mx-5">
-              <p>25&deg;C</p>
+              <p>{{ Weather.highTemp }}&deg;C</p>
               <p>
-                18&deg;C
+                {{ Weather.lowTemp }}&deg;C
                 <!-- <img src="./assets/logo.png" /> -->
               </p>
             </div>
@@ -41,11 +42,11 @@
         <div class="card-bottom px-5 py-4 row">
           <!-- starts here -->
           <div class="col text-center">
-            <p>23&deg;C</p>
+            <p>{{ Weather.feelsLike }}&deg;C</p>
             <span>feels like</span>
           </div>
           <div class="col text-center">
-            <p>55%</p>
+            <p>{{ Weather.humidity }}%</p>
             <span>humidity</span>
           </div>
         </div>
@@ -58,7 +59,32 @@
 <script>
 export default {
   name: "App",
-  data() {},
+  data() {
+    return {
+      citySearch: "",
+      Weather: {
+        cityName: "Gwarinpa",
+        country: "NG",
+        temperature: 12,
+        description: "Clouds everywhere",
+        lowTemp: "19",
+        highTemp: "30",
+        feelsLike: "20",
+        humidity: "55",
+      },
+    };
+  },
+  methods: {
+    async getWeather() {
+      console.log(this.citySearch);
+      const key = "2cfe5922ed02db8c9175de7178fb0bf8";
+      const baseURL = `https://api.openweathermap.org/data/2.5/weather?q=${this.citySearch}&appid=${key}`;
+      const response = await fetch(baseURL);
+      const data = await response.json();
+      console.log(data);
+      this.citySearch = "";
+    },
+  },
 };
 </script>
 
