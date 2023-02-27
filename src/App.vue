557edfa8bb792c830acbe265cce228f2ea9fc16f
prathima <kanadamprathima@gmail.com>
2023-02-27 16:24:33 +0100

    initialised vue and installed bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container my-5">
+    <h1 class="title text-center">Weather Forecast</h1>
+    <form>
+      <input
+        type="text"
+        placeholder="Enter location"
+        auto-complete="on"
+        class="form-control text-muted form-rounded p-3 shadow"
+      />
+    </form>
+    <div class="card rounded my-4 shadow-lg back-card overflow:hidden">
+      <!-- top of card starts here -->
+      <div class="card-top text-center" style="margin-bottom: 15rem">
+        <div class="city-name my-3">
+          <p>INDIA</p>
+          <span>....</span>
+          <p class="">IND</p>
+        </div>
+      </div>
+      <!-- card middle body -->
+      <div class="card-body">
+        <div class="card-mid">
+          <div class="row">
+            <div class="col-12 text-center temp">
+              <span>20&deg;C</span>
+              <p class="my-4">clouds everywhere</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col d-flex justify-content-between px-5 mx-5">
+              <p>25&deg;C</p>
+              <p>
+                18&deg;C
+                <!-- <img src="./assets/logo.png" /> -->
+              </p>
+            </div>
+          </div>
+        </div>
+        <!-- bottom card -->
+        <div class="card-bottom px-5 py-4 row">
+          <!-- starts here -->
+          <div class="col text-center">
+            <p>23&deg;C</p>
+            <span>feels like</span>
+          </div>
+          <div class="col text-center">
+            <p>55%</p>
+            <span>humidity</span>
+          </div>
+        </div>
+        <!-- card ends here -->
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {},
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./assets/styles.css";
 </style>
